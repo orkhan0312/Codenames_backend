8967f049d63b590ca690c029ea51b2071d381d4e
Orkhan Gahramanov <orxan03121@gmail.com>
2021-05-12 22:35:17 +0400

    Create game side added

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CardController extends Controller
+{
+    public function get(Request $request)
+    {
+        try {
+            $boards = DB::table('cards')
+                ->leftJoin('words', 'cards.word_id', '=', 'words.id')
+                ->leftJoin('colors', 'cards.color_id', '=', 'colors.id')
+                ->select('cards.*' , 'words.en as word_en', 'colors.color as color')
+                ->get();
+            return parent::asJson($boards);
+        } catch (\Exception $exception) {
+            return parent::asJson(null,'TRACE_ERROR', $exception->getMessage());
+        }
+    }
+
+    public function create(Request $request)
+    {
+        try {
+            $first = rand(0,1)==1;
+            DB::table('games')->where('id', $request->input('game_id'))
+                ->update(['is_red_first' => $first]);
+            if($first == '1') {
+                $colors = collect([1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4]);
+            } else {
+                $colors = collect([1, 1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4]);
+            }
+            $colors = $colors->shuffle();
+            $words = collect(range(1, 25));
+            $words = $words->shuffle();
+            for ($x = 0; $x < 5; $x++) {
+                for ($y = 0; $y < 5; $y++) {
+                    $raw = DB::table('cards')->insert(['i' => $x, 'j' => $y, 'color_id' => $colors->pop(),
+                        'game_id' => $request->input('game_id'), 'word_id' => $words->pop()]);
+                }
+            }
+            return parent::asJson($first);
+        } catch (\Exception $exception) {
+            return parent::asJson(null,'TRACE_ERROR', $exception->getMessage());
+        }
+    }
+}
---
+++ database/migrations/2021_05_12_124557_create_cards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('word_id');
+            $table->integer('game_id');
+            $table->integer('color_id');
+            $table->integer('i');
+            $table->integer('j');
+            $table->integer('opened_by')->nullable();
+            $table->integer('opened_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
